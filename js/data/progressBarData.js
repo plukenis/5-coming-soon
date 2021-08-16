441322437b277e3ecba26fe7755ca7da4bdde061
plukenis <ieva.zaboole@gmail.com>
2021-08-16 16:41:12 +0300

    progress bars data

---
+++ js/data/progressBarData.js
@@ -0,0 +1,20 @@
+const progressBarData = [
+    {
+        label: 'UX Design',
+        value: 90,
+    },
+    {
+        label: 'Web Design',
+        value: 86,
+    },
+    {
+        label: 'Web Development',
+        value: 50,
+    },
+    {
+        label: 'Web Errors',
+        value: 5,
+    },
+];
+
+export {progressBarData}
