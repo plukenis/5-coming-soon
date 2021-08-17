c9751bf2676b65b827905d0d7152bdc1e2bb468a
plukenis <ieva.zaboole@gmail.com>
2021-08-17 12:52:21 +0300

    progress bar validation

--- js/components/progress-bar/ProgressBars.js
+++ js/components/progress-bar/ProgressBars.js
@@ -1,31 +1,105 @@
 class ProgressBars {
     constructor(selector, data) {
-        this.selector = selector
-        this.data = data
-
+        this.selector = selector;
+        this.data = data;
+        this.DOM = null;
+        this.init();
+    }
+    init() {
+        if (!this.isValidSelector() ||
+            !this.isValidData()) {
+            return false;
+        }
+        if (!this.findTargetElement()) {
+            console.error('ERROR: pagal pateikta this.selector nepavyko rasti norimo elemento');
+            return false;
+        }
         this.render();
+    }
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+            this.selector === '') {
+            console.error('ERROR: this.selector turi buti ne tuscias tekstas');
+            return false;
+        }
+        return true;
+    }
+    isValidData() {
+        if (!Array.isArray(this.data) ||
+            this.data.length === 0) {
+            console.error('ERROR: this.data turi buti ne tuscias array');
+            return false;
+        }
+        return true;
+    }
+    findTargetElement() {
+        this.DOM = document.querySelector(this.selector);
+        return !!this.DOM;
+    }
+
+    isValidDataItem(item) {
+        const allowedKeys = ['label', 'value'];
+
+        // tikriname, jog item yra objektas
+        if (typeof item !== 'object' ||
+            Array.isArray(item) ||
+            item === null) {
+            console.warn('WARN: netinkamas tipas');
+            return false;
+        }
+
+        // tikriname, ar item.label yra tinkamas
+        if (typeof item.label !== 'string' ||
+            item.label === '') {
+            console.warn('WARN: netinkamas item.label');
+            return false;
+        }
+
+        // tikriname, ar item.value yra tinkamas
+        if (typeof item.value !== 'number' ||
+            !isFinite(item.value) ||
+            item.value < 0 ||
+            item.value > 100) {
+            console.warn('WARN: netinkamas item.label');
+            return false;
+        }
+
+        // tikriname, ar item objektas neturi per daug key's (raktazodziu)
+        for (const key in item) {
+            if (!allowedKeys.includes(key)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     render() {
         let HTML = '';
 
-        for (let i = 0; i < this.data.length; i++) {
-            const item = this.data[i];
-            HTML += ` <div class="progress-bar">
-                         <div class="top">
+        for (const item of this.data) {
+            if (!this.isValidDataItem(item)) {
+                continue;
+            }
+
+            HTML += `<div class="progress-bar">
+                        <div class="top">
                             <div class="label">${item.label}</div>
                             <div class="value">${item.value}%</div>
                         </div>
                         <div class="bottom">
-                            <div class="progress" style="width: ${item.value}%;"></div> 
-                        </div> 
+                            <div class="progress" style="width: ${item.value}%;"></div>
+                        </div>
                     </div>`;
         }
-        
-        const DOM = document.querySelector(this.selector);
-        DOM.innerHTML += HTML; 
+
+        if (HTML === '') {
+            console.error('ERROR: this.data neturi nei vienos validzios reiksmes');
+            return false;
+        }
+
+        this.DOM.innerHTML += HTML;
     }
 }
 
-
-export { ProgressBars } 
+export { ProgressBars }
--- js/data/progressBarData.js
+++ js/data/progressBarData.js
@@ -2,19 +2,66 @@ const progressBarData = [
     {
         label: 'UX Design',
         value: 90,
+        hack: true,
     },
+    {
+        label: 'UX Design',
+        value: 90,
+    },
+    'ertdyfh',
+    ['ewre', 452],
+    true,
+    null,
+    false,
+    () => { },
+    {
+        label: 'UX Design',
+        value: -90,
+    },
+    {
+        label: 'UX Design',
+        value: Infinity,
+    },
+    {
+        label: 'UX Design',
+        value: -Infinity,
+    },
+    {
+        label: 'UX Design',
+        value: NaN,
+    },
+    {
+        label: 'UX Design',
+        value: 990,
+    },
+    {
+        label: 'UX Design',
+        value: 77.7,
+    },
+    {},
+    {
+        label: 'labas :P',
+    },
+    {
+        value: 'labas :P',
+    },
+    5847152,
     {
         label: 'Web Design',
         value: 86,
     },
     {
+        label: 'Web Design',
+        value: '86',
+    },
+    {
+        label: [1, 2, 5],
+        value: 'wewrt',
+    },
+    {
         label: 'Web Development',
         value: 50,
     },
-    {
-        label: 'Web Errors',
-        value: 5,
-    },
 ];
 
-export {progressBarData}
+export { progressBarData }
